66e9171ead90f3c83543d86bfc4cf1f90e539ce5
vosure <vosure1@gmail.com>
2020-02-18 21:55:11 +0300

    initial commit. finished the series

---
+++ ColorGenerator.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorGenerator
+{
+    ColorSettings settings;
+    Texture2D texture;
+    const int textureResolution = 50;
+    INoiseFilter biomenoiseFilter;
+
+
+    public void UpdateSettings(ColorSettings settings)
+    {
+        this.settings = settings;
+        if (texture == null || texture.height != settings.biomeColorSettings.bioms.Length)
+        {
+            texture = new Texture2D(textureResolution * 2, settings.biomeColorSettings.bioms.Length, TextureFormat.RGBA32, false);
+        }
+        biomenoiseFilter = NoiseFilterFactory.CreateNoiseFilter(settings.biomeColorSettings.noise);
+    }
+
+    public void UpdateElevation(MinMax elevationMinMax)
+    {
+        settings.planetMaterial.SetVector("_elevationMinMax", new Vector4(elevationMinMax.Min, elevationMinMax.Max));
+    }
+
+    public float BiomePercentFromPoint(Vector3 pointOnUnitSphere)
+    {
+        float heightPercent = (pointOnUnitSphere.y + 1) / 2.0f;
+        heightPercent += (biomenoiseFilter.Evaluate(pointOnUnitSphere) - settings.biomeColorSettings.noiseOffset) * settings.biomeColorSettings.noiseStrength;
+        float biomeIndex = 0;
+        int numBiomes = settings.biomeColorSettings.bioms.Length;
+        float blendRange = settings.biomeColorSettings.blendAmount / 2.0f + 0.001f;
+
+        for (int i = 0; i < numBiomes; i++)
+        {
+            float dst = heightPercent - settings.biomeColorSettings.bioms[i].startHeight;
+            float weigth = Mathf.InverseLerp(-blendRange, blendRange, dst);
+            biomeIndex *= (1 - weigth);
+            biomeIndex += i * weigth;
+        }
+
+        return biomeIndex / Mathf.Max(1, numBiomes - 1);
+    }
+
+    public void UpdateColors()
+    {
+        Color[] colors = new Color[texture.width * texture.height];
+        int colorIndex = 0;
+        foreach (var biome in settings.biomeColorSettings.bioms)
+        {
+            for (int i = 0; i < textureResolution * 2; i++)
+            {
+                Color gradientColor;
+                if (i < textureResolution)
+                {
+                    gradientColor = settings.oceanColor.Evaluate(i / (textureResolution - 1.0f));
+                }
+                else
+                {
+                    gradientColor = biome.gradient.Evaluate((i - textureResolution) / (textureResolution - 1.0f));
+                }      
+                Color tintColor = biome.tint;
+                colors[colorIndex] = gradientColor * (1 - biome.tintPercent) + tintColor * biome.tintPercent;
+                colorIndex++;
+            }
+        }
+        texture.SetPixels(colors);
+        texture.Apply();
+        settings.planetMaterial.SetTexture("_texture", texture);
+    }
+}
---
+++ TerrainFace.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainFace
+{
+    ShapeGenerator shapeGenerator;
+    Mesh mesh;
+    int resolution;
+    Vector3 localUp;
+    Vector3 axisA;
+    Vector3 axisB;
+
+    public TerrainFace(ShapeGenerator shapeGenerator, Mesh mesh, int resolution, Vector3 localUp)
+    {
+        this.shapeGenerator = shapeGenerator;
+        this.mesh = mesh;
+        this.resolution = resolution;
+        this.localUp = localUp;
+
+        axisA = new Vector3(localUp.y, localUp.z, localUp.x);
+        axisB = Vector3.Cross(localUp, axisA);
+    }
+
+    public void ConstructMesh()
+    {
+        Vector3[] vertices = new Vector3[resolution * resolution];
+        int[] triangles = new int[(resolution - 1) * (resolution - 1) * 6];
+
+        int triangleIndex = 0;
+
+        Vector2[] uv = (mesh.uv.Length == vertices.Length) ? mesh.uv : new Vector2[vertices.Length];
+
+        for (int y = 0; y < resolution; y++)
+        {
+            for (int x = 0; x < resolution; x++)
+            {
+                int i = x + y * resolution;
+                Vector2 percent = new Vector2(x, y) / (resolution - 1);
+                Vector3 pointOnUnitCube = localUp + (percent.x - 0.5f) * 2 * axisA + (percent.y - 0.5f) * 2 * axisB;
+                Vector3 pointOnUnitSphere = pointOnUnitCube.normalized;
+                float unscaledElevation = shapeGenerator.CalculateUnscaledElevation(pointOnUnitSphere);
+                vertices[i] = pointOnUnitSphere * shapeGenerator.GetScaledElevation(unscaledElevation);
+                uv[i].y = unscaledElevation;
+
+                if (x != resolution - 1 && y != resolution - 1)
+                {
+                    triangles[triangleIndex] = i;
+                    triangles[triangleIndex + 1] = i + resolution + 1;
+                    triangles[triangleIndex + 2] = i + resolution;
+
+                    triangles[triangleIndex + 3] = i;
+                    triangles[triangleIndex + 4] = i + 1;
+                    triangles[triangleIndex + 5] = i + resolution + 1;
+
+                    triangleIndex += 6;
+                }
+            }
+        }
+
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+        mesh.uv = uv;
+    }
+
+    public void UpdateUVs(ColorGenerator colorGenerator)
+    {
+        Vector2[] uv = mesh.uv;
+        for (int y = 0; y < resolution; y++)
+        {
+            for (int x = 0; x < resolution; x++)
+            {
+                int i = x + y * resolution;
+                Vector2 percent = new Vector2(x, y) / (resolution - 1);
+                Vector3 pointOnUnitCube = localUp + (percent.x - 0.5f) * 2 * axisA + (percent.y - 0.5f) * 2 * axisB;
+                Vector3 pointOnUnitSphere = pointOnUnitCube.normalized;
+
+                uv[i].x = colorGenerator.BiomePercentFromPoint(pointOnUnitSphere);
+            }
+        }
+
+        mesh.uv = uv;
+    }
+}
